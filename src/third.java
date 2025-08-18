8e6cc82a80b8073100bb6281a19f07090bdef12a
Robeco945 <carettoroberto0312@gmail.com>
2025-08-18 13:26:07 +0300

    changes from floats to doubles

--- src/third.java
+++ src/third.java
@@ -6,13 +6,13 @@ public class third {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Give the first number:");
-        float first = Integer.parseInt(scanner.nextLine());
+        double first = Integer.parseInt(scanner.nextLine());
 
         System.out.println("Give the second number:");
-        float second = Integer.parseInt(scanner.nextLine());
+        double second = Integer.parseInt(scanner.nextLine());
 
         System.out.println("Give the third number:");
-        float third = Integer.parseInt(scanner.nextLine());
+        double third = Integer.parseInt(scanner.nextLine());
 
         System.out.println("The sum of the numbers is: " + (first + second + third));
         System.out.println("the product of the numbers is:" + (first*second*third));
@@ -20,5 +20,5 @@ public class third {
     }
 }
 /*The avg was returning as 4 instead of 4.3333
-so i made the inputs into floats so that the output is not rounded
+so i made the inputs into doubles so that the output is not rounded
  */
