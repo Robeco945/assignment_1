d146be0491337c02ca904bdb2e7d6f5cb1b985a4
Robeco945 <carettoroberto0312@gmail.com>
2025-08-18 14:03:33 +0300

    1.2.2

---
+++ 1.2/src/second.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+import static java.lang.Math.sqrt;
+
+public class second {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the first leg of the triangle:");
+        double a = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("enter the second leg of the triangle:");
+        double b = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("The hypotenuse of your triangle is: " + (Math.sqrt(a * a + b * b)));
+    }
+}
