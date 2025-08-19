1dc0d4bcdb56e92873a614e32b217d3ad0ea19d5
Robeco945 <carettoroberto0312@gmail.com>
2025-08-19 11:50:57 +0300

    1.3.2. not done, stuck iterating

--- 1.3/src/binary.java
+++ 1.3/src/binary.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
 public class binary {
-}
+    public static void main(String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("give me a binary: ");
+        String bina = (scanner.nextLine());
+        int i = bina.length();
+        while (i>=0) {
+            int j = Integer.parseInt(String.valueOf(bina.charAt(i-1)));
+
+            //int decimal =
+            System.out.println("i: "+i);
+            System.out.println("j:"+j);
+            i--;
+        }
+
+
+        }
+    }
