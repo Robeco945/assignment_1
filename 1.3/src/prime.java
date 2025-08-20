daf840f0d219646efd52d0fc2b66e69cd871b0b1
Robeco945 <carettoroberto0312@gmail.com>
2025-08-20 23:54:13 +0300

    1.3.3 started not done

--- 1.3/src/prime.java
+++ 1.3/src/prime.java
@@ -1,22 +1,27 @@
 import java.util.Scanner;
 public class prime {
-    public static void main(String[] arg){
+    public static void main(String[] arg) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the starting number");
         int start = Integer.parseInt((scanner.nextLine()));
         System.out.println("Enter the ending number (must be larger than the starting number");
         int end = Integer.parseInt((scanner.nextLine()));
-        while (end<=start){
+        while (end <= start) {
             System.out.println("please enter a number that is larger than the staring number");
             end = Integer.parseInt((scanner.nextLine()));
-            if (end > start){
+            if (end > start) {
                 break;
             }
         }
-        int n = start;
-        while (n <= end) {
+        int n = start - 1;
+        while (n < end) {
+            //for (int i = 1; i < 10; i++) {
+              //  if (n % i == 0 && n != i) {
+                n++;
+                System.out.println("n   "+n);
+                //System.out.println(i);
+             }
+                }
 
-            n++;
-        }
-    }
-}
+            }
+
