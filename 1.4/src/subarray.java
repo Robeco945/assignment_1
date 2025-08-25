24314c2379ae59c1ae8ce8100488d4dee9ef5332
Robeco945 <carettoroberto0312@gmail.com>
2025-08-25 22:46:27 +0300

    1.4.2 done

--- 1.4/src/subarray.java
+++ 1.4/src/subarray.java
@@ -5,15 +5,29 @@ public class subarray {
         System.out.print("enter the array length: ");
         int length = scanner.nextInt();
         int[] numbers = new int[length];
-        for (int i = 0; i<length; i++) {
-            int max = Integer.MIN_VALUE;
-            for (int start = 0; start < numbers.length; start++) {
-                for (int end = start; end < numbers.length; end++) {
-
-                }
-            }
-            System.out.print("Enter character number "+ (i+1) + ": ");
+        int max = 0;
+        int maxstart = 0;
+        int maxend = 0;
+        int cursum = 0;
+        for (int i = 0; i < length; i++) {
+            System.out.print("Enter character number " + (i + 1) + ": ");
             numbers[i] = scanner.nextInt();
         }
+            max = Integer.MIN_VALUE;
+            for (int start = 0; start < numbers.length; start++) {
+                cursum = 0;
+                for (int end = start; end < numbers.length; end++) {
+                    cursum += numbers[end];
+                    if (cursum > max) {
+                        max = cursum;
+                        maxstart = start;
+                        maxend = end;
+                    }
+                }
+            }
+        System.out.println("max sum is: " + max);
+            System.out.println("Integer: " + (maxstart+1) + "-" + (maxend+1));
     }
-}
+    }
+
+
