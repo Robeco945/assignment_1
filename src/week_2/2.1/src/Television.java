3f7b57c3cbbd92a6d35de43603c97355d9d7b610
Robeco945 <carettoroberto0312@gmail.com>
2025-08-26 15:22:38 +0300

    mending the location and starting 2.1

---
+++ src/week_2/2.1/src/Television.java
@@ -0,0 +1,23 @@
+public class Television {
+    private boolean isOn;
+    private int Channel;
+
+
+    public Television(boolean isOn, int Channel){
+        this.isOn = isOn;
+        this.Channel = Channel;
+    }
+    Television television = new Television(true,1);
+    public int getChannel(){
+        return Channel;
+    }
+    public boolean getIsOn(){
+        return isOn;
+    }
+    public setChannel(int){
+        Channel = Channel;
+    public void new Television()
+
+        }
+    }
+}
