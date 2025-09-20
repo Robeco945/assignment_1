bbbf336228cce9c9a3150e958de668594f691f15
Robeco945 <carettoroberto0312@gmail.com>
2025-09-20 15:37:50 +0300

    done 5.2

--- src/week_5/5.2/src/Ticket.java
+++ src/week_5/5.2/src/Ticket.java
@@ -1,6 +1,43 @@
+class Customer{
+    static int number = 0;
+    int id;
+
+    public Customer(){
+        synchronized (Customer.class){
+        number += 1;
+        id = number;
+    }
+    }
+
+    public void reserve(int tickets){
+        synchronized(Ticket.class) {
+            int result = Ticket.seats - tickets;
+            if (result >= 0) {
+                Ticket.seats = result;
+                System.out.print("Customer " + id + " purchased " + tickets + " tickets");
+                System.out.println("(remains: " + Ticket.seats + " tickets)");
+            } else {
+                System.out.println("Customer " + id + " could not purchase " + tickets + " tickets");
+            }
+        }
+    }
+}
 public class Ticket {
+    public static int seats = 10;
     public static void main(String[] arg){
-System.out.println("no tickets for you");
-        System.out.print("yes tickets for you");
+        Customer c1 = new Customer();
+        Customer c2 = new Customer();
+        Customer c3 = new Customer();
+        Customer c4 = new Customer();
+
+        Thread t1 = new Thread(() -> c1.reserve(2));
+        Thread t2 = new Thread(() -> c2.reserve(6));
+        Thread t3 = new Thread(() -> c3.reserve(3));
+        Thread t4 = new Thread(() -> c4.reserve(2));
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
     }
 }
